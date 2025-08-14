bc191be5a1844820193b8b67b0855875d4ddb2c3
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-08-14 20:33:52 -0300

    feat: hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,11 @@ function Home() {
   return (
     <>
       <Header />
+      <section className="hero" aria-label="Banner">
+        <h1 className="hero__title">Venha conhecer nossas promoções</h1>
+        <h2 className="hero__subtitle"><span>50% off </span>nos produtos</h2>
+        <a href="#" className="hero__button">Ver produto</a>
+      </section>
     </>
   )
 }
