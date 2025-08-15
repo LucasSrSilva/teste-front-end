99dfc9b3deb6557c6065cd495b7b4126a9ecf167
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-08-14 21:49:42 -0300

    feat: initial category section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header"
+import Icon from "../components/ui/Icon"
 
 
 function Home() {
@@ -10,6 +11,19 @@ function Home() {
         <h1 className="hero__title">Venha conhecer nossas promoções</h1>
         <h2 className="hero__subtitle"><span>50% off </span>nos produtos</h2>
         <a href="#" className="hero__button">Ver produto</a>
+      </section>
+      <section className="category" aria-label="Categorias">
+        <nav className="category__navigation">
+          <ul>
+            <li><a id="tecnology" href="#"><Icon size={61} hidden src="/icons/categories/tecnology.svg" /></a><label htmlFor="tecnology">Tecnologia</label></li>
+            <li><a id="supermarket" href="#"><Icon size={61} hidden src="/icons/categories/supermarket.svg" /></a><label htmlFor="supermarket">Supermercado</label></li>
+            <li><a id="drinks" href="#"><Icon size={61} hidden src="/icons/categories/drinks.svg" /></a><label htmlFor="drinks">Bebidas</label></li>
+            <li><a id="tools" href="#"><Icon size={61} hidden src="/icons/categories/tools.svg" /></a><label htmlFor="tools">Ferramentas</label></li>
+            <li><a id="health" href="#"><Icon size={61} hidden src="/icons/categories/health.svg" /></a><label htmlFor="health">Saúde</label></li>
+            <li><a id="sports" href="#"><Icon size={61} hidden src="/icons/categories/sports.svg" /></a><label htmlFor="sports">Esportes e Fitness</label></li>
+            <li><a id="fashion" href="#"><Icon size={61} hidden src="/icons/categories/fashion.svg" /></a><label htmlFor="fashion">Moda</label></li>
+          </ul>
+        </nav>
       </section>
     </>
   )
