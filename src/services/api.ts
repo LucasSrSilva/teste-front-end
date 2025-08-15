25fc3862e0b3ade756e8a5e8370094cd56f8d745
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-08-15 13:11:43 -0300

    fix: fake no browser req

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,15 @@
-const BASE_URL = "https://app.econverse.com.br" 
-/*Por ser apenas um teste técnico não adicionei como 
-variável de ambiente, para facilitar iniciar o projeto*/
 export const api = {
     async get(path: string) {
-        const response = await fetch(`${BASE_URL}${path}`)
-        if(!response.ok) {
-            throw new Error(`Erro ao buscar: ${response.statusText}`)
+        const response = await fetch(`/api${path}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
-        return response.json();
+        })
+        if(!response.ok) {
+                throw new Error(`Erro ao buscar: ${response.statusText}`)
+            }
+            const data = response.json()
+        return data;
     }
 }
