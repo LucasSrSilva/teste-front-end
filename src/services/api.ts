b0b2eb3931e6acb48abe474a7f841604900baaf0
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-08-17 13:54:51 -0300

    fix: fake req for blocked cors working in prod

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,30 @@
+const BASE_URL = "https://app.econverse.com.br" 
+/*Por ser apenas um teste técnico não adicionei como 
+variável de ambiente, para facilitar o inicio do projeto*/
+
 export const api = {
     async get(path: string) {
-        const response = await fetch(`/api${path}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+        try{
+            const response = await fetch(`${BASE_URL}${path}`, {
+            method: 'GET',
+            })
+            if(!response.ok) {
+                    throw new Error(`Erro ao buscar: ${response.statusText}`)
+                }
+                const data =await response.json()
+                return data;
+                
+            }catch{
+                //como a api está bloqueada pelo cors, a forma mais simples de
+                //testar é colocando uma copia do json na pasta public
+                const localResponse = await fetch(`/products.json`, {
+                method: 'GET',
+                })
+                if(!localResponse.ok) {
+                        throw new Error(`Erro ao buscar: ${localResponse.statusText}`)
+                    }
+                    const data = await localResponse.json()
+                    return data;
         }
-        })
-        if(!response.ok) {
-                throw new Error(`Erro ao buscar: ${response.statusText}`)
-            }
-            const data = response.json()
-        return data;
     }
 }
